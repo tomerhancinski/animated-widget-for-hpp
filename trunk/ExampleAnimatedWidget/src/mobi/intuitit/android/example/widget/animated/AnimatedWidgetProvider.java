d5ce61f2ddb4c1114cc76082ca56c0f982de763e
wang.b.frank <wang.b.frank@c006d53c-d5d5-11de-8017-1f6ee2e63914>
2009-11-20 13:10:04 +0000

    Initial import, Eclipse 3.5 project

---
+++ trunk/ExampleAnimatedWidget/src/mobi/intuitit/android/example/widget/animated/AnimatedWidgetProvider.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2009 Bo Wang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mobi.intuitit.android.example.widget.animated;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+/**
+ * Simple example for a frame animation and a tween animation
+ * 
+ * @author bo
+ * 
+ */
+public class AnimatedWidgetProvider extends AppWidgetProvider {
+
+    public static final String ACTION_CLICK = AnimatedWidgetProvider.class.getPackage().toString()
+            + ".ACTION_CLICK";
+
+    public static final String EXTRA_APPWIDGET_ID = AnimatedWidgetProvider.class.getPackage()
+            .toString()
+            + ".APPWIDGET_ID";
+    public static final String EXTRA_VIEW_ID = AnimatedWidgetProvider.class.getPackage().toString()
+            + ".VIEW_ID";
+
+    static final String TAG = "Animated Widget Example";
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Log.i(TAG, action + "");
+
+        if (ACTION_CLICK.equals(action)) {
+            onClick(context, intent.getIntExtra(EXTRA_APPWIDGET_ID, -1), intent.getIntExtra(
+                    EXTRA_VIEW_ID, -1));
+        } else if (HppIntent.NOTIFICATION.NOTIFICATION_FRAME_ANIMATION_STARTED.equals(action)) {
+            onAnimStarted(intent);
+        } else if (HppIntent.NOTIFICATION.NOTIFICATION_FRAME_ANIMATION_STOPPED.equals(action)) {
+            onAnimStopped(intent);
+        } else if (HppIntent.ERROR.ERROR_FRAME_ANIMATION.equals(action)) {
+            Log.e(TAG, "Frame Anim: " + intent.getStringExtra(HppIntent.EXTRA.EXTRA_ERROR_MESSAGE));
+        } else if (HppIntent.ERROR.ERROR_TWEEN_ANIMATION.equals(action)) {
+            Log.e(TAG, "Tween Anim: " + intent.getStringExtra(HppIntent.EXTRA.EXTRA_ERROR_MESSAGE));
+        } else if (HppIntent.NOTIFICATION.NOTIFICATION_IN_VIEWPORT.equals(action)) {
+
+        } else if (HppIntent.NOTIFICATION.NOTIFICATION_OUT_VIEWPORT.equals(action)) {
+
+        } else
+            super.onReceive(context, intent);
+    }
+
+    private void onAnimStarted(Intent intent) {
+
+    }
+
+    private void onAnimStopped(Intent intent) {
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // Construct views
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main);
+
+        // Instead of being put it in xml, the animation is assigned here to
+        // show you that it can be dynamically changed. For example, in a
+        // weather widget, cloudy->sunny->rainy can be done by two animations.
+        views.setImageViewResource(R.id.anim_view, R.anim.muybridge);
+
+        // Tell the widget manager
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            int appWidgetId = appWidgetIds[i];
+            setOnClickListener(context, appWidgetId, views, R.id.btn_start);
+            setOnClickListener(context, appWidgetId, views, R.id.btn_stop);
+            setOnClickListener(context, appWidgetId, views, R.id.btn_shake);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    /**
+     * Set pending intent in remote views
+     * 
+     * @param context
+     * @param views
+     *            Content remote views
+     * @param viewId
+     *            The view listening to click
+     */
+    void setOnClickListener(Context context, int appWidgetId, RemoteViews views, int viewId) {
+        Intent active = new Intent(context, AnimatedWidgetProvider.class);
+
+        active.setAction(ACTION_CLICK);
+
+        active.putExtra(EXTRA_APPWIDGET_ID, appWidgetId);
+        active.putExtra(EXTRA_VIEW_ID, viewId);
+
+        // This is tricky, be aware that you can only have one set of extras for
+        // any given PendingIntent action+data+category+component pair.
+        active.setData(Uri.parse(System.currentTimeMillis() + ""));
+
+        PendingIntent actionPendingIntent = PendingIntent.getBroadcast(context, 0, active,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+        views.setOnClickPendingIntent(viewId, actionPendingIntent);
+    }
+
+    /**
+     * Callback
+     * 
+     * @param viewId
+     */
+    void onClick(Context context, int appWidgetId, int viewId) {
+
+        switch (viewId) {
+        case R.id.btn_start:
+            // Log.i("===", "");
+            context.sendBroadcast(new Intent(HppIntent.ACTION.ACTION_START_FRAME_ANIMATION).putExtra(
+                    HppIntent.EXTRA.EXTRA_APPWIDGET_ID, appWidgetId)
+                    .putExtra(HppIntent.EXTRA.EXTRA_IMAGEVIEW_ID, R.id.anim_view));
+            break;
+        case R.id.btn_stop:
+            context.sendBroadcast(new Intent(HppIntent.ACTION.ACTION_STOP_FRAME_ANIMATION).putExtra(
+                    HppIntent.EXTRA.EXTRA_APPWIDGET_ID, appWidgetId)
+                    .putExtra(HppIntent.EXTRA.EXTRA_IMAGEVIEW_ID, R.id.anim_view));
+            break;
+        case R.id.btn_shake:
+            context.sendBroadcast(new Intent(HppIntent.ACTION.ACTION_START_TWEEN_ANIMATION).putExtra(
+                    HppIntent.EXTRA.EXTRA_APPWIDGET_ID, appWidgetId)
+                    .putExtra(HppIntent.EXTRA.EXTRA_VIEW_ID, R.id.text_hello)
+                    .putExtra(HppIntent.EXTRA.EXTRA_ANIMATION_ID, R.anim.shake));
+            break;
+        default:
+        }
+    }
+
+}
